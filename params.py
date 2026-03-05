f447ad1c66e9bd893a4d386bb3638d5d9a6a6010
gao634 <gao634@purdue.edu>
2026-03-05 16:53:47 -0500

    vis

--- params.py
+++ params.py
@@ -182,7 +182,7 @@ goal_qc = Position(
 #batch_size = 65536
 batch_size = 8192
 seed = 0
-time_to_evolve = 20
+time_to_evolve = 40
 # Safe params
 
 # callables_MCP = Callables(
@@ -215,7 +215,7 @@ sim_params_DI = MJXparams(
     bounds = Bounds(),
     dims=6,
     action_dims=3,
-    dt = 0.05,
+    dt = 0.025,
     seed = seed
 )
 
